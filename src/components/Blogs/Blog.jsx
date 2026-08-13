c3f28d9e17f5ee933f2a2bd9eea286e8a61731df
Mahfuj <mdmahfuj.cse@gmail.com>
2026-08-13 12:17:29 +0600

    show bookmarks

--- src/components/Blogs/Blog.jsx
+++ src/components/Blogs/Blog.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoBookmarksOutline } from "react-icons/io5";
 
 const Blog = ({ blog }) => {
   //const {blog}=props;
@@ -10,8 +11,17 @@ const Blog = ({ blog }) => {
           <img className="w-48 h-32 object-cover rounded-lg mx-2 my-2" src={blog.cover} alt="Movie" />
         </figure>
         <div className="card-body">
+          <div className="author flex justify-around items-center">
+            <h3>{blog.author}</h3>
+            <img className="w-12" src={blog.author_img} ></img>
+          <IoBookmarksOutline />
+
+          </div>
           <h2 className="card-title">{blog.title}</h2>
           <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          {
+            blog.hashtags.map((has)=> <p>{has}</p>)
+          }
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Book Mark</button>
           </div>
